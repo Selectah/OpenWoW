e6908efc0e40d8b6a9509daba47f970fdece04e4
caffeine239 <caffeine239@yahoo.com>
2013-11-14 21:13:26 -0500

    Fixed Realm Loop

--- src/server/game/Handlers/AuthHandler.cpp
+++ src/server/game/Handlers/AuthHandler.cpp
@@ -31,61 +31,62 @@ void WorldSession::SendAuthResponse(uint8 code, bool queued, uint32 queuePos)
     }
     
     WorldPacket packet(SMSG_AUTH_RESPONSE, 80);
+	packet << uint8(code = AUTH_OK);
     packet.WriteBit(queued);
-    packet.WriteBit(code == AUTH_OK);
-	
-    packet << uint8(code);                             // Auth response ?
 
     if (queued)
-    {
-        packet.WriteBit(1);                             // Unknown
-    }
-
+        packet.WriteBit(1);
+		packet.WriteBit(code = AUTH_OK);
+		
     if (code == AUTH_OK)
     {
         packet.WriteBit(0);
-        packet.WriteBits(0, 21);
-        packet.WriteBits(0, 21);
-        packet.WriteBits(result->GetRowCount(), 23);
-        packet.WriteBit(0);
-        packet.WriteBit(0);
-        packet.WriteBit(0);
+		packet.WriteBits(0, 21);
+		packet.WriteBits(0, 21);
         packet.WriteBits(result2->GetRowCount(), 23);
+        packet.WriteBit(0);
+        packet.WriteBit(0);
+        packet.WriteBit(0);
+        packet.WriteBits(result->GetRowCount(), 23);
+
+        packet.FlushBits();
     }
-    TC_LOG_ERROR(LOG_FILTER_GENERAL, "SMSG_AUTH_RESPONSE");
-    packet.FlushBits();
-    
+
     if (code == AUTH_OK)
     {
-        packet << uint32(0);
+		packet << uint32(0);
         packet << uint32(0);
         packet << uint8(Expansion());
 
-        do
-        {
-            Field* fields = result->Fetch();
-
-            packet << fields[1].GetUInt8();
-            packet << fields[0].GetUInt8();
-        } 
-        while (result->NextRow());        
-
-        packet << uint8(Expansion());
-        packet << uint32(0);
-
-        do
+		do
         {
             Field* fields = result2->Fetch();
 
-            packet << fields[0].GetUInt8();
             packet << fields[1].GetUInt8();
+            packet << fields[0].GetUInt8();
         } 
         while (result2->NextRow());
 
+		packet << uint8(Expansion());
+		packet << uint32(0);
+        
+
+		    do
+        {
+            Field* fields = result->Fetch();
+
+            packet << fields[0].GetUInt8();
+            packet << fields[1].GetUInt8();
+        } 
+        while (result->NextRow()); 
 
         packet << uint32(0);
         packet << uint32(0);
+
     }
+
+	if (queuePos)
+    packet << uint8(code);
 
     SendPacket(&packet);
 }
